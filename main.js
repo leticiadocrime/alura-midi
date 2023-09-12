e6eb859124950691216bdee386183901b3637fbb
leticiadocrime <106694092+leticiadocrime@users.noreply.github.com>
2023-09-12 13:13:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-<audio scr="sounds/keyq.wav" id="som_tecla_pom"></audio>
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
